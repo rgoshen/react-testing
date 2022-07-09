a0fc16157ba44116887fbc5b1e43bc29775a27c6
Richard Goshen <rick.goshen@gmail.com>
2022-07-09 07:33:37 -0700

    fix actors comma

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -7,26 +7,39 @@ import {
   BACKDROP_SIZE,
   BASE_IMAGE_URL,
 } from '../utilities/appConstants';
+import formatDate from '../utilities/formatDate';
 import './MovieDetail.css';
 
 export default function MovieDetail() {
   const { id } = useParams();
   const [movie, setMovie] = useState({});
+  const [actors, setActors] = useState([]);
 
-  const getMovieDetail = async () => {
+  const getMovie = async () => {
     try {
       const res = await fetch(BASE_URL + `movie/${id}` + API_KEY);
-      const detail = await res.json();
-      setMovie(detail);
+      const newMovie = await res.json();
+      setMovie(newMovie);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getActors = async () => {
+    try {
+      const res = await fetch(BASE_URL + `movie/${id}/credits` + API_KEY);
+      const credits = await res.json();
+      setActors(credits.cast.slice(0, 4));
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    getMovieDetail();
+    getMovie();
+    getActors();
     window.scrollTo(0, 0);
-  }, []);
+  }, [id]);
 
   if (!movie.title) return null;
 
@@ -50,6 +63,18 @@ export default function MovieDetail() {
             {movie.genres.map((genre) => (
               <span key={genre.id}>{genre.name} * </span>
             ))}
+            <span>
+              {formatDate(movie.release_date) + ' * ' + movie.runtime + ' mins'}
+            </span>
+          </p>
+          <p className='movie-detail-info-cast'>
+            {actors.map((actor, index) =>
+              index !== actors.length - 1 ? (
+                <span key={actor.credit_id}>{actor.name + ', '}</span>
+              ) : (
+                <span key={actor.credit_id}>{actor.name}</span>
+              )
+            )}
           </p>
         </div>
       </div>
