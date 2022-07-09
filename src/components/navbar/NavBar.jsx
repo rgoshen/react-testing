c301ea78a18725af946ae33306beb6fa7ebb91b7
Richard Goshen <rick.goshen@gmail.com>
2022-07-09 15:36:58 -0700

    add movie logo and remove words in navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,8 +4,8 @@ import './NavBar.css';
 export default function NavBar() {
   return (
     <nav className='nav'>
-      <Link to='/' className='nav-site-title'>
-        Home
+      <Link to='/' className='nav-brand'>
+        <img src={process.env.PUBLIC_URL + 'movies-list-logo.png'} alt='logo' />
       </Link>
       <ul className='nav-bar'>
         <li className='nav-element active'>
