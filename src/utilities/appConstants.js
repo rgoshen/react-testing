24bd0d8bbb08cc01c74f5ef582276bd885b44e51
Richard Goshen <rick.goshen@gmail.com>
2022-07-08 22:57:13 -0700

    add more constants

--- src/utilities/appConstants.js
+++ src/utilities/appConstants.js
@@ -1,4 +1,7 @@
-const API_KEY = process.env.REACT_APP_MOVIE_API;
+const API_KEY = `?api_key=${process.env.REACT_APP_MOVIE_API}`;
 const BASE_URL = 'https://api.themoviedb.org/3/';
+const POSTER_SIZE = 'original';
+const BACKDROP_SIZE = 'original';
+const BASE_IMAGE_URL = 'http://image.tmdb.org/t/p/';
 
-export { API_KEY, BASE_URL };
+export { API_KEY, BASE_URL, POSTER_SIZE, BACKDROP_SIZE, BASE_IMAGE_URL };
